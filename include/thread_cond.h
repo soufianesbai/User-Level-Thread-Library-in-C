82bba966180083dde35e8f3f40df39e29218d04a
rafiq amrar <ramrar@bordeaux-inp.fr>
2026-04-04 14:58:00 +0200

    fix

--- include/thread_cond.h
+++ include/thread_cond.h
@@ -3,6 +3,8 @@
 
 #include "thread.h"
 
+#ifndef USE_PTHREAD
+
 typedef struct {
     struct thread_queue waiting_queue;
 } thread_cond_t;
@@ -12,5 +14,16 @@ int thread_cond_wait(thread_cond_t *cond, thread_mutex_t *mutex);
 int thread_cond_signal(thread_cond_t *cond);
 int thread_cond_broadcast(thread_cond_t *cond);
 int thread_cond_destroy(thread_cond_t *cond);
+
+#else
+
+#define thread_cond_t pthread_cond_t
+#define thread_cond_init(_cond) pthread_cond_init((_cond), NULL)
+#define thread_cond_wait pthread_cond_wait
+#define thread_cond_signal pthread_cond_signal
+#define thread_cond_broadcast pthread_cond_broadcast
+#define thread_cond_destroy pthread_cond_destroy
+
+#endif
 
 #endif
--- include/thread_sem.h
+++ include/thread_sem.h
@@ -3,6 +3,8 @@
 
 #include "thread.h"
 
+#ifndef USE_PTHREAD
+
 typedef struct {
     int count;
     struct thread_queue waiting_queue;
@@ -12,5 +14,17 @@ int thread_sem_init(thread_sem_t *sem, int value);
 int thread_sem_wait(thread_sem_t *sem);
 int thread_sem_post(thread_sem_t *sem);
 int thread_sem_destroy(thread_sem_t *sem);
+
+#else
+
+#include <semaphore.h>
+
+#define thread_sem_t sem_t
+#define thread_sem_init(_sem, _value) sem_init((_sem), 0, (_value))
+#define thread_sem_wait sem_wait
+#define thread_sem_post sem_post
+#define thread_sem_destroy sem_destroy
+
+#endif
 
 #endif
