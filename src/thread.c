3bef1b93cfa61e11dabe3df6c5e07fd7623dcad8
Soufiane SBAI <soufiane.sbai@bordeaux-inp.fr>
2026-04-27 10:38:16 +0200

    fix

--- src/thread.c
+++ src/thread.c
@@ -43,8 +43,6 @@ static int head_ref_active_index = 0;
 static struct stack_entry deferred_stacks[MAX_DEFERRED_STACKS];
 static int deferred_stack_count = 0;
 
-#define STACK_PREFILL_COUNT 0
-
 static void reclaim_deferred_stacks_batch(int budget) {
   while (deferred_stack_count > 0 && budget-- > 0) {
     struct stack_entry e = deferred_stacks[--deferred_stack_count];
@@ -212,16 +210,6 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
 #ifdef ENABLE_PREEMPTION
     init_prem(preemption_handler, 100);
 #endif
-
-    /*
-     * Pre-fill the stack pool so early thread_create() calls can reuse
-     * mappings instead of paying mmap()+mprotect() on the critical path.
-     */
-    for (int i = 0; i < STACK_PREFILL_COUNT; i++) {
-      struct stack_entry e;
-      if (stack_pool_alloc(&e) == 0)
-        stack_pool_push(&e);
-    }
 
     scheduler_initialized = 1;
   }
